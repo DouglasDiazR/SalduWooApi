304d9b7a683de2558cd35e7a022b39b160b3e945
DouglasDiaz <161756607+DouglasDiazR@users.noreply.github.com>
2024-11-20 20:33:00 -0500

    creadas rutas para buscar y filtrar  ordenes fecha  por id y ordenes por porducto

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,25 +1,81 @@
-import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common'
+import {
+    Controller,
+    Get,
+    HttpCode,
+    Param,
+    Query,
+    Req,
+    UseGuards,
+} from '@nestjs/common'
 import { OrdersService } from './orders.service'
 import { AuthGuard } from 'src/guards/auth.guard'
 import { Roles } from 'src/decorators/roles.decorator'
 import { Role } from 'src/enum/role.enum'
-import { query, Request } from 'express'
+import { Request } from 'express'
 
 @Controller('orders')
 export class OrdersController {
     constructor(private readonly ordersService: OrdersService) {}
 
     @Get()
-    async getOrders() {
-        return await this.ordersService.getOrders()
+    @HttpCode(200)
+    /*     @UseGuards(AuthGuard)
+    @Roles(Role.Admin) */
+    async getOrders(
+        @Query('startDate') startDate?: string,
+        @Query('endDate') endDate?: string,
+    ) {
+        return await this.ordersService.getOrders({ startDate, endDate })
     }
 
+    @Get('byProduct')
+    @HttpCode(200)
+    /*     @UseGuards(AuthGuard)
+    @Roles(Role.Admin) */
+    async getOrdersByProduct(
+        @Query('startDate') startDate?: string,
+        @Query('endDate') endDate?: string,
+        @Query('idProduct') idProduct?: number,
+    ) {
+        return await this.ordersService.getOrdersByProduct({
+            startDate,
+            endDate,
+            idProduct,
+        })
+    }
+
+    @Get()
+    @HttpCode(200)
+    @UseGuards(AuthGuard)
+    @Roles(Role.Admin)
+    async getOrdersById(
+        @Query('startDate') startDate?: string,
+        @Query('endDate') endDate?: string,
+    ) {
+        return await this.ordersService.getOrders({ startDate, endDate })
+    }
+
+    @Get('ordersUser')
+    @HttpCode(200)
     @UseGuards(AuthGuard)
     @Roles(Role.Seller)
-    @Get('ordersUser')
-    async getOrdersUser(@Req() req: Request) {
+    async getOrdersUser(
+        @Query('startDate') startDate: string,
+        @Query('endDate') endDate: string,
+        @Query('idProduct') idProduct: number,
+        @Req() req: Request,
+    ) {
         const idWooUser = req.user.id_wooCommerce
         console.log('id_wooUser', idWooUser)
-        return await this.ordersService.getOrdersUser(idWooUser)
+        return await this.ordersService.getOrdersUser(
+            { startDate, endDate, idProduct },
+            idWooUser,
+        )
+    }
+
+    @Get(':id')
+    @HttpCode(200)
+    async getOrderById(@Param('id') id: number) {
+        return await this.ordersService.getOrderById(id)
     }
 }
