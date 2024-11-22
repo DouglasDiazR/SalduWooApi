204004f9f745095e406c9354a8fffa04cbe18fd6
Santiago Gama <131696138+Gamma1404@users.noreply.github.com>
2024-11-22 13:04:06 -0500

    feat(app.service): add onApplicationBootstrap function to initialize WooCommerce data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common'
+import { WooCommerceService } from './wooApi/wooApi.service'
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!'
+    constructor(private readonly wooCommerceService: WooCommerceService) {}
+    async onApplicationBootstrap() {
+        await this.wooCommerceService.getUsers()
+        await this.wooCommerceService.getProducts()
     }
 }
--- src/wooApi/wooApi.module.ts
+++ src/wooApi/wooApi.module.ts
@@ -4,13 +4,12 @@ import { WooCommerceService } from './wooApi.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { WooCommerceConfig } from 'src/config/wooCommerce'
 import { Products } from 'src/entities/products.entity'
-import { UsersRepository } from 'src/users/users.repository'
 import { Users } from 'src/entities/users.entity'
 
 @Module({
     imports: [TypeOrmModule.forFeature([Users, Products])],
     controllers: [WooCommerceController],
-    providers: [WooCommerceService, WooCommerceConfig, UsersRepository],
+    providers: [WooCommerceService, WooCommerceConfig],
     exports: [WooCommerceService],
 })
 export class WooApiModule {}
