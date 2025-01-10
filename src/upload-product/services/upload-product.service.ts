b24d4d86557cd4d62bc739eb2910d0d208e5ef36
Charly Correa M. <chuck.scrumdev@gmail.com>
2025-01-10 12:34:21 -0500

    Methods for upload product entity managements

--- src/upload-product/services/upload-product.service.ts
+++ src/upload-product/services/upload-product.service.ts
@@ -1,25 +1,78 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UploadProduct } from 'src/entities/upload-product.entity';
-import { Repository } from 'typeorm';
-import { CreateUploadProductDTO, UpdateUploadProductDTO } from '../dtos/upload-product.dto';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UploadProduct } from 'src/entities/upload-product.entity'
+import { Repository } from 'typeorm'
+import {
+    CreateUploadProductDTO,
+    UpdateUploadProductDTO,
+} from '../dtos/upload-product.dto'
 
 @Injectable()
 export class UploadProductService {
     constructor(
         @InjectRepository(UploadProduct)
-        private uploadProductRepository: Repository<UploadProduct>
+        private uploadProductRepository: Repository<UploadProduct>,
     ) {}
 
-    async findAll() {}
+    async findAll(providerId?: number, uploadStatus?: string) {
+        let queryBuilder = this.uploadProductRepository
+            .createQueryBuilder('uploadProduct')
+            .orderBy('uploadProduct.createdAt', 'DESC')
+        if (uploadStatus !== undefined) {
+            queryBuilder = queryBuilder.where(
+                'uploadProduct.status = :uploadStatus',
+                {
+                    uploadStatus,
+                },
+            )
+        }
+        if (providerId !== undefined) {
+            queryBuilder = queryBuilder.andWhere(
+                'uploadProduct.providerId = :providerId',
+                {
+                    providerId,
+                },
+            )
+        }
+        return await queryBuilder.getMany()
+    }
 
-    async findOne(id: number) {}
+    async findOne(id: number) {
+        const product = await this.uploadProductRepository
+            .createQueryBuilder('uploadProduct')
+            .where('uploadProduct.id = :id', { id })
+            .getOne()
+        if (!product) {
+            throw new NotFoundException(
+                `The Product with ID: ${id} was Not Found`,
+            )
+        }
+        return product
+    }
 
-    async findAllByProviderId(providerId: number, uploadStatus?: string) {}
+    async createEntity(payload: CreateUploadProductDTO) {
+        const newProduct = this.uploadProductRepository.create(payload)
+        return await this.uploadProductRepository.save(newProduct)
+    }
 
-    async createEntity(payload: CreateUploadProductDTO) {}
+    async updateEntity(id: number, payload: UpdateUploadProductDTO) {
+        const product = await this.findOne(id)
+        if (!product) {
+            throw new NotFoundException(
+                `The Product with ID: ${id} was Not Found`,
+            )
+        }
+        this.uploadProductRepository.merge(product, payload)
+        return await this.uploadProductRepository.save(product)
+    }
 
-    async updateEntity(id:number, payload: UpdateUploadProductDTO) {}
-    
-    async deleteEntity(id: number) {}
+    async deleteEntity(id: number) {
+        const product = await this.findOne(id)
+        if (!product) {
+            throw new NotFoundException(
+                `The Product with ID: ${id} was Not Found`,
+            )
+        }
+        return await this.uploadProductRepository.softDelete(product.id)
+    }
 }
